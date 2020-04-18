29596fd21e13162aa87458fe254fd377276ecb21
bibhuyash <vibhakaryashaswi@gmail.com>
2020-04-18 11:48:29 +0530

    Moving Private information to dev.js

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const { User } = require("./model/user");
+const config = require("./config/key");
 
 const app = express();
+
 mongoose
-  .connect(
-    "mongodb+srv://bibhuyash:Avichal.28@cluster0-i1ot5.mongodb.net/test?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
+  .connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("DB Connected"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.post("/api/users/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userdata) => {
+    if (err) return res.json({ sucess: false, err });
+  });
+  return res.status(200).json({
+    sucess: true,
+  });
 });
 
 app.listen(5000, () => {
